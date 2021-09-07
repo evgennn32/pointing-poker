35bc07619aa3a4e6ccb3218ef7309baf7acf1416
KorvinAtreides <Korvin1312b@yandex.by>
2021-09-07 15:15:18 +0300

    feat: add main logo

--- src/component/MainPage/MainPage.tsx
+++ src/component/MainPage/MainPage.tsx
@@ -7,14 +7,25 @@ const Main = styled.main`
 `;
 
 const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
   width: 100%;
   max-width: 1000px;
   min-height: 100px;
-  margin: 0 auto;
+  margin: 40px auto 0;
+`;
+
+const MainLogo = styled.img`
+  width: 90%;
+  max-width: 550px;
 `;
 
 export const MainPage = (): JSX.Element => (
   <Main>
-    <Content>form</Content>
+    <Content>
+      <MainLogo src="./images/MainLogo.png" />
+    </Content>
   </Main>
 );
