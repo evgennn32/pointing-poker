97d14e0cbaf6fc8ebf0f939a446a5d470fd6101d
KorvinAtreides <Korvin1312b@yandex.by>
2021-09-08 16:27:33 +0300

    feat: add ticking timer

--- src/component/Timer/Timer.tsx
+++ src/component/Timer/Timer.tsx
@@ -9,17 +9,53 @@ type Props = {
   readOnly: boolean;
 };
 
-const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-  console.log(e.target.value);
-  e.target.value = String(Number(e.target.value));
-  if (Number(e.target.value) > Number(e.target.max)) {
-    e.target.value = e.target.max;
+const changeHandler = (value: string, max: string) => {
+  let currentValue = String(Number(value));
+  if (Number(currentValue) > Number(max)) {
+    currentValue = max;
   }
-  if (Number(e.target.value) <= 0) e.target.value = "0";
-  if (Number(e.target.value) < 10) e.target.value = `0${e.target.value}`;
+  if (Number(currentValue) <= 0) currentValue = "0";
+  if (Number(currentValue) < 10) currentValue = `0${currentValue}`;
+  return currentValue;
 };
-
+/* eslint max-lines-per-function: 0 */
 const Timer = ({ readOnly }: Props): JSX.Element => {
+  const [minutesValue, setMinutesValue] = React.useState("02");
+  const [secondsValue, setSecondsValue] = React.useState("30");
+  const [currentValue, setCurrentValue] = React.useState("150");
+  const [ticking, setTicking] = React.useState(true);
+
+  React.useEffect(() => {
+    setCurrentValue(String(60 * Number(minutesValue) + Number(secondsValue)));
+    console.log(currentValue);
+  }, [minutesValue, secondsValue]);
+
+  React.useEffect(() => {
+    if (ticking) {
+      const tickInterval = setInterval(() => {
+        if (secondsValue === "00") {
+          setSecondsValue("60");
+          const newMinutes = changeHandler(
+            String(Number(minutesValue) - 1),
+            "5",
+          );
+          setMinutesValue(newMinutes);
+        } else {
+          const newSeconds = changeHandler(
+            String(Number(secondsValue) - 1),
+            "60",
+          );
+          setSecondsValue(newSeconds);
+        }
+        if (secondsValue === "00" && minutesValue === "00") {
+          setTicking(false);
+          clearInterval(tickInterval);
+          console.log("off");
+        }
+        console.log("tick", secondsValue);
+      }, 1000);
+    }
+  }, [ticking]);
   return (
     <TimerWrapper>
       <TimerMinutesInput
@@ -28,7 +64,11 @@ const Timer = ({ readOnly }: Props): JSX.Element => {
         max="5"
         step="1"
         type="number"
-        onChange={(e) => changeHandler(e)}
+        value={minutesValue}
+        onChange={(e) => {
+          const updatedValue = changeHandler(e.target.value, e.target.max);
+          setMinutesValue(updatedValue);
+        }}
       />
       <TimerSecondsInput
         readOnly={readOnly}
@@ -36,7 +76,11 @@ const Timer = ({ readOnly }: Props): JSX.Element => {
         max="60"
         step="1"
         type="number"
-        onChange={(e) => changeHandler(e)}
+        value={secondsValue}
+        onChange={(e) => {
+          const updatedValue = changeHandler(e.target.value, e.target.max);
+          setSecondsValue(updatedValue);
+        }}
       />
     </TimerWrapper>
   );
