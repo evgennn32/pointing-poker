549585b01b667030aeea63d161ef1415fcf414c1
Yauheni Saladukha <evgennn32@gmail.com>
2021-09-09 16:56:05 +0300

    feat: add title component

---
+++ src/component/Title/Title.styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 71px;
+  width: 100%;
+`;
+
+export const TitleContent = styled.h2`
+  font-size: 24px;
+  font-family: Ruda-Bold, sans-serif;
+  line-height: 30px;
+`;
