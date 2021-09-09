b0341fe52e083156e6494f4618d87a8b69168378
KorvinAtreides <Korvin1312b@yandex.by>
2021-09-09 16:11:28 +0300

    feat: add chat wrapper styles

--- src/component/Chat/Chat.styled.ts
+++ src/component/Chat/Chat.styled.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { ButtonStyled } from "../Button/Button";
 
 export const ChatWrapper = styled.aside<{ active: boolean }>`
   position: sticky;
@@ -6,10 +7,26 @@ export const ChatWrapper = styled.aside<{ active: boolean }>`
   top: 0;
   right: 20px;
   ${(props) => (props.active ? "display: inline-block;" : "display: none;")}
+  width: 350px;
+  height: 700px;
+  max-height: 80vh;
   border: 1px solid black;
-  width: 300px;
-  height: 600px;
-  max-height: 60vh;
+  border-radius: 3px;
 `;
 
-export const ChatEnterWrapper = styled.div``;
+export const ChatHistoryWrapper = styled.div`
+  width: 100%;
+  height: calc(100% - 50px);
+`;
+export const ChatEnterWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 50px;
+  border-top: 1px solid black;
+  border-radius: 3px;
+`;
+export const EnterButton = styled(ButtonStyled)`
+  width: 66px;
+  height: 48px;
+`;
--- src/component/Chat/Chat.tsx
+++ src/component/Chat/Chat.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
-import { ChatEnterWrapper, ChatWrapper } from "./Chat.styled";
+import { Input } from "../styledComponents/Input/Input";
+import {
+  ChatEnterWrapper,
+  ChatHistoryWrapper,
+  ChatWrapper,
+  EnterButton,
+} from "./Chat.styled";
 
 const Chat = (): JSX.Element => {
   const chatActive = useSelector((state: RootState) => state.chat.isActive);
   return (
     <ChatWrapper active={chatActive}>
-      <ChatEnterWrapper />
+      <ChatHistoryWrapper />
+      <ChatEnterWrapper>
+        <Input width="230" />
+        <EnterButton isLight={true}>➔</EnterButton>
+      </ChatEnterWrapper>
     </ChatWrapper>
   );
 };
