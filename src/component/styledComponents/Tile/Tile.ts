ae773e62447f1ed29361947cc5a069c811fbba6e
Yauheni Saladukha <evgennn32@gmail.com>
2021-09-10 11:22:05 +0300

    feat: add tile stiled component

---
+++ src/component/styledComponents/Tile/Tile.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Tile = styled.div`
+  width: 300px;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  justify-content: space-around;
+  background-color: white;
+  -webkit-box-shadow: 0 5px 10px 2px rgba(34, 60, 80, 0.2);
+  -moz-box-shadow: 0 5px 10px 2px rgba(34, 60, 80, 0.2);
+  box-shadow: 0 5px 10px 2px rgba(34, 60, 80, 0.2);
+  border-radius: 5px;
+  margin: 20px;
+`;
