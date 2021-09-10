818ed60cef3e84a60f91b16c53035cf2da99fcb1
Yauheni Saladukha <evgennn32@gmail.com>
2021-09-10 14:11:02 +0300

    fix: members style

--- src/component/Members/Members.styled.ts
+++ src/component/Members/Members.styled.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 export const MembersWrapper = styled.div`
   display: flex;
   width: 100%;
-  justify-content: space-around;
+  justify-content: flex-start;
   gap: 15px;
+  flex-wrap: wrap;
 `;
