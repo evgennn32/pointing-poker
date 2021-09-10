8f1feaac33a95ea7887d2c0b76ddf8c599027d2e
Yauheni Saladukha <evgennn32@gmail.com>
2021-09-10 14:00:02 +0300

    feat: add User interface to UserAvatar component

--- src/component/UserAvatar/UserAvatar.tsx
+++ src/component/UserAvatar/UserAvatar.tsx
@@ -7,18 +7,11 @@ import {
   SmallTxt,
   Wrapper,
 } from "./UserAvatar.styled";
+import User from "../../models/User";
 
-type UserAvatarProps = {
-  image: string | null;
-  firstName: string;
-  lastName: string;
-  position: string;
-  currentUser: boolean;
-};
-
-export const UserAvatar = (props: UserAvatarProps): JSX.Element => {
+export const UserAvatar = (props: User): JSX.Element => {
   return (
-    <Wrapper>
+    <>
       <Avatar avatar={props.image}>
         {props.firstName !== "" && props.image === null && (
           <Initials>{props.firstName[0]}</Initials>
@@ -32,26 +25,28 @@ export const UserAvatar = (props: UserAvatarProps): JSX.Element => {
         <Name>{props.firstName + " " + props.lastName}</Name>
         {props.position !== "" && <SmallTxt>{props.position}</SmallTxt>}
       </NameAndPosition>
-      <Button>
-        <svg
-          version="1.1"
-          id="Capa_1"
-          x="0px"
-          y="0px"
-          viewBox="0 0 65.518 65.518"
-          xmlSpace="preserve"
-        >
-          <g>
-            <path
-              fill="grey"
-              d="M32.759,0C14.696,0,0,14.695,0,32.759s14.695,32.759,32.759,32.759s32.759-14.695,32.759-32.759S50.822,0,32.759,0z
-		 M6,32.759C6,18.004,18.004,6,32.759,6c6.648,0,12.734,2.443,17.419,6.472L12.472,50.178C8.443,45.493,6,39.407,6,32.759z
-		 M32.759,59.518c-5.948,0-11.447-1.953-15.895-5.248l37.405-37.405c3.295,4.448,5.248,9.947,5.248,15.895
-		C59.518,47.514,47.514,59.518,32.759,59.518z"
-            />
-          </g>
-        </svg>
-      </Button>
-    </Wrapper>
+      {props.ableToDelete && (
+        <Button>
+          <svg
+            version="1.1"
+            id="Capa_1"
+            x="0px"
+            y="0px"
+            viewBox="0 0 65.518 65.518"
+            xmlSpace="preserve"
+          >
+            <g>
+              <path
+                fill="grey"
+                d="M32.759,0C14.696,0,0,14.695,0,32.759s14.695,32.759,32.759,32.759s32.759-14.695,32.759-32.759S50.822,0,32.759,0z
+       M6,32.759C6,18.004,18.004,6,32.759,6c6.648,0,12.734,2.443,17.419,6.472L12.472,50.178C8.443,45.493,6,39.407,6,32.759z
+       M32.759,59.518c-5.948,0-11.447-1.953-15.895-5.248l37.405-37.405c3.295,4.448,5.248,9.947,5.248,15.895
+      C59.518,47.514,47.514,59.518,32.759,59.518z"
+              />
+            </g>
+          </svg>
+        </Button>
+      )}
+    </>
   );
 };
