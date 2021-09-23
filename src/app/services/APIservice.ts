43c5dc2497c55f1ce12301b71d2043919bd82438
IamDarya <75080122+IamDarya@users.noreply.github.com>
2021-09-23 17:36:17 +0200

    feat: add reconnection to server

--- src/app/services/APIservice.ts
+++ src/app/services/APIservice.ts
@@ -2,6 +2,10 @@ import { io } from "socket.io-client";
 
 export const APIservice = {
   connect: () => {
-    return io("http://localhost:4000/");
+    return io("http://localhost:4000/", {
+      reconnection: true,
+      reconnectionDelay: 500,
+      reconnectionAttempts: Infinity,
+    });
   },
 };
