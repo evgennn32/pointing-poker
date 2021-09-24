bf4739ea70a0e559ea15949fdc3e5be6d1541428
Yauheni Saladukha <evgennn32@gmail.com>
2021-09-24 15:19:07 +0300

    refactor: merge

--- src/app/services/APIservice.ts
+++ src/app/services/APIservice.ts
@@ -1,11 +1,22 @@
 import { io } from "socket.io-client";
+import User from "../../models/User";
 
 export const APIService = {
+  socket: {},
   connect: () => {
-    return io("http://localhost:4000/", {
+    APIService.socket = io("http://localhost:4000/", {
       reconnection: true,
       reconnectionDelay: 500,
       reconnectionAttempts: Infinity,
     });
+  },
+  gameCreate: async (user: User) => {
+    try {
+      APIService.socket.emit('create:game', user, (res) => {
+        console.log(res)
+      });
+    } catch (e) {
+
+    }
   },
 };
