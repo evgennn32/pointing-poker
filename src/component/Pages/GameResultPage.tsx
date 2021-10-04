98dc2d9aafdde4d536fa153152a4c492734d4f12
IamDarya <75080122+IamDarya@users.noreply.github.com>
2021-10-04 18:06:48 +0200

    download result (need help)

--- src/component/Pages/GameResultPage.tsx
+++ src/component/Pages/GameResultPage.tsx
@@ -6,6 +6,8 @@ import { VoteResults } from "../VoteResults/VoteResults";
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import { GameRoomEntity } from "../../models/GameRoomEntity";
+import { Button } from "../Button/Button";
+import voteValue from "../VoteResults/VoteResults.test.value";
 
 const GameResultPage = (): JSX.Element => {
   const game = useSelector<RootState, GameRoomEntity>(
@@ -22,6 +24,45 @@ const GameResultPage = (): JSX.Element => {
           (issue) => ` ${issue.issueName}`,
         )})`}
       />
+      <Button
+        isLightTheme={false}
+        textContent={"Download in .cvs"}
+        onClick={() => {
+          const head = "Issue Name, Priority, Type, Short Type, Value, Percent";
+          let csv = "";
+          let csvv = "";
+          game.gameResults.forEach((el) => {
+            csv += el.issue.issueName.concat(",", el.issue.priority);
+            el.voteResults.forEach((el) => {
+              csvv += el.card.type
+                .concat(",", el.card.value)
+                .concat(",", el.value);
+            });
+            csv += "," + csvv;
+            csv += "\n";
+          });
+          // voteValue.issues.forEach(function (el) {
+          //   csv += el.issueName.concat(",", el.priority);
+          //   statisticsObserver(el.results).forEach((el) => {
+          //     csvv +=
+          //       el.shortType
+          //         .concat(",", el.type)
+          //         .concat(",", el.shortType)
+          //         .concat(",", el.value)
+          //         .concat(",", el.percent.toString()) + ",";
+          //   });
+          //   csv += "," + csvv;
+          //   csv += "\n";
+          // });
+          csv = head + "\n" + csv;
+          console.log(csv);
+          const hiddenElement = document.createElement("a");
+          hiddenElement.href = "data:text/csv;charset=utf-8," + encodeURI(csv);
+          hiddenElement.target = "_blank";
+          hiddenElement.download = "data.csv";
+          hiddenElement.click();
+        }}
+      ></Button>
       {game.rounds.map((result, index) => {
         const issue = game.issues.find((issue) => result.issueId === issue.id);
         return (
